2208421674ac7ea625c7cc1b2104f4fb44d793f8
justin <276887367@qq.com>
2014-10-12 22:26:28 +0800

    timefmt error
    print_record_time函数中设置的时间格式为"%d/%m/%y-%R"，因此sh中的format少了%y

--- tools/cpu.sh
+++ tools/cpu.sh
@@ -21,7 +21,7 @@ set xlabel "time"
 set ylabel "percent (%)"
 set yrange [ 0 : 100 ]
 set xdata time
-set timefmt "%d/%m-%H:%M"
+set timefmt "%d/%m/%y-%H:%M"
 set format x "%H:%M"
 plot "$datafile" using 1:2 title "user" with lines, "$datafile" using 1:3 title "sys" with lines, "$datafile" using 1:4 title "wait" with lines, "$datafile" using 1:5 title "util" with lines
 
